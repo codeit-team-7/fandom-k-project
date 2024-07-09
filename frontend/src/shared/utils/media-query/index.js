d388baac5278f268af4e080b411e5bdd96bf58ef
김우성 <me02years@gmail.com>
2024-07-09 18:42:51 +0900

    fix : pixel-calculator JSDOC 추가

--- frontend/src/shared/utils/media-query/index.js
+++ frontend/src/shared/utils/media-query/index.js
@@ -2,6 +2,11 @@ import { css } from "styled-components";
 import { BREAKPOINTS } from "@app/theme";
 import { pxe, px } from "@utils";
 
+/***
+ * 미디어 쿼리 함수
+ * @param {string} string - 해당 템플릿 문자열을 각 브레이크 포인트에 맞게 변경합니다
+ * @returns {string} 변경된 템플릿 리터럴
+ */
 export const media = Object.keys(BREAKPOINTS).reduce((acc, label) => {
   return (
     (acc[label] = (...args) => css`
--- frontend/src/shared/utils/pxcel-calculator/index.js
+++ frontend/src/shared/utils/pxcel-calculator/index.js
@@ -1,9 +1,26 @@
+// 베이스 픽셀
 const BASE_PIXEL = 16;
 
+/***
+ * 픽셀 값을 BASE_PIXEL값으로 나누어 rem단위로 변환합니다.
+ * @param {number} px 변환 할 px값
+ * @returns {string} 변환 된 rem값
+ */
 export const pxr = (px) => `${px / BASE_PIXEL}rem`;
 
+/***
+ * 픽셀 값을 BASE_PIXEL값으로 나누어 em단위로 변환합니다.
+ * @param {number} px 변환할 px값
+ * @returns 변환 된 em값
+ */
 export const pxe = (px) => `${px / BASE_PIXEL}em`;
 
+/**
+ * 들어온 값들 중, 값의 타입이 숫자라면 해당 숫자를 REM단위로 변경하는 템플릿 리터럴 함수.
+ * @param {string} strings 템플릿 리터럴의 문자열 부분
+ * @param {...*} values 템플릿 리터럴에 삽입된 표현식 값들
+ * @returns {string} 처리된 결과 문자열
+ */
 export const px = (strings, ...values) => {
   return strings.reduce((acc, str, i) => {
     const value = values[i];
