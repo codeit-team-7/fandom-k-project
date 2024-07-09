d2c1dd58f5ed3d04e36b3d2dd3be4f3e1a1c4e6b
김우성 <me02years@gmail.com>
2024-07-09 18:57:25 +0900

    feat : theme 추가 및 FONT_SIZE 추가, 이미지 추가

--- frontend/src/app/theme/index.js
+++ frontend/src/app/theme/index.js
@@ -1,17 +1,47 @@
 export const BREAKPOINTS = {
-  default: 1,
-  extra_small: 320,
-  small: 425,
-  medium: 728,
-  large: 1024,
-  extra_large: 1440,
+  BASE: 1,
+  XSM: 320,
+  SM: 425,
+  MD: 728,
+  LG: 1024,
 };
 
-export const COLORS = {};
+// 컬러
+export const COLORS = {
+  BLACK: {
+    200: "#02000e",
+    100: "#181d26",
+  },
+  GRAY: {
+    300: "#67666e",
+    200: "#828282",
+    100: "#A3A5A8",
+  },
+  WHITE: {
+    100: "#f7f7f8",
+    200: "8c92ab",
+  },
+  BRAND: {
+    100: "#f96d69",
+    200: "#fe5493",
+  },
+};
 
-export const FONT_SIZES = {};
+// 폰트 사이즈
+export const FONT_SIZES = {
+  "3xSM": 12,
+  "2xSM": 13,
+  "XSM": 14,
+  "SM": 15,
+  "MD": 16,
+  "LG": 18,
+  "XLG": 20,
+  "2xLG": 26,
+};
 
+// 테마
 export const theme = {
   ...BREAKPOINTS,
   ...COLORS,
+  ...FONT_SIZES,
 };
--- frontend/src/shared/utils/media-query/index.js
+++ frontend/src/shared/utils/media-query/index.js
@@ -4,10 +4,10 @@ import { pxe, px } from "@utils";
 
 /***
  * 미디어 쿼리 함수
- * @param {string} string - 해당 템플릿 문자열을 각 브레이크 포인트에 맞게 변경합니다
+ * @param {string} string 해당 템플릿 문자열을 각 브레이크 포인트에 맞게 변경합니다
  * @returns {string} 변경된 템플릿 리터럴
  */
-export const media = Object.keys(BREAKPOINTS).reduce((acc, label) => {
+export const Media = Object.keys(BREAKPOINTS).reduce((acc, label) => {
   return (
     (acc[label] = (...args) => css`
       @media (min-width: ${pxe(BREAKPOINTS[label])}) {
