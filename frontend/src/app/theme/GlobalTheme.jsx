4aaf636f7130a737693e1f1dbcc3b963a746dcdd
homerabbit <92077507+me92years@users.noreply.github.com>
2024-07-10 19:02:26 +0900

    fix : Globaltheme box-sizing추가, Button 수정 (#4)

--- frontend/src/app/theme/GlobalTheme.jsx
+++ frontend/src/app/theme/GlobalTheme.jsx
@@ -6,6 +6,12 @@ const GlobalTheme = createGlobalStyle`
   ${reset}
 
   ${({ theme }) => `
+    *,
+    *::before,
+    *::after {
+      box-sizing: border-box;
+    }
+
     body {
       width: 100%;
       min-height: 100vh;
