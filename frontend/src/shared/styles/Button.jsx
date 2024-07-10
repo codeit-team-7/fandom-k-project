d9ee849b935b8bc218e1de1b95434a90fe1a0950
kimsayhi <kimse418@gmail.com>
2024-07-10 16:27:54 +0900

    feat : Button 기본 스타일 컴포넌트 구현

---
+++ frontend/src/shared/styles/Button.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { media } from "@utils";
+
+export const Button = styled.button`
+  ${({ theme, $activable }) =>
+    `${media.base`
+  background: ${
+    $activable
+      ? `${theme.colors.GRAY[200]};`
+      : `linear-gradient(to right, ${theme.colors.BRAND[100]}, ${theme.colors.BRAND[200]}`
+  });
+  color: #fff;
+  width: 100%;
+  height:${42};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: ${theme.fontSize.XSM};
+  font-weight: 700;
+  border: none
+  `}`}
+`;
+
+// props 전달하지않을 경우 기본색입니다
+// 회색버튼 필요시 $activable로 truthy 값을 전달하시면 됩니다.
