98a3cc023a54a53f3fcb3d58818e4906549207b1
kimsayhi <kimse418@gmail.com>
2024-07-10 18:51:35 +0900

    feat : 모달창 Layout, Background 스타일 구현

---
+++ frontend/src/shared/styles/ModalLayout.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { media } from "@utils";
+/**모달창 기본레이아웃입니다.
+width와 height는 직접 추가,
+모달 최하단이 버튼이 아니라면 padding-bottom에 대한 수정이 필요함*/
+export const ModalLayout = styled.div`
+  ${({ theme }) =>
+    `${media.base`
+ position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding-top: ${24};
+  padding-left: ${24}; 
+  padding-right: ${24}; 
+  padding-bottom: ${32}; 
+  background-color: ${theme.colors.BLACK[100]};
+  z-index: 11;
+  `}`}
+`;
