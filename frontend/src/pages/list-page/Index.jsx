00dd1e594283e0d63c4040408fa0a22acde9aadc
전주현 <scv903@naver.com>
2024-07-11 19:06:58 +0900

    feat : 내크레딧 섹션, 크레딧 충전 모달창, 헤더 컴포넌트 개발

--- frontend/src/pages/list-page/Index.jsx
+++ frontend/src/pages/list-page/Index.jsx
@@ -1,8 +1,19 @@
-import { FavoriteIdol } from "@features";
+import { FavoriteIdol, Credit, ChargeCredit } from "@features";
+import { useState } from "react";
+import { Header } from "../../features";
 
 export default function Index() {
+  const [chargeModal, setChargeModal] = useState(false);
+
+  const handleChargeModal = () => {
+    setChargeModal(chargeModal ? false : true);
+  };
+
   return (
     <main>
+      {chargeModal && <ChargeCredit onChargeClick={handleChargeModal} />}
+      <Header />
+      <Credit onChargeClick={handleChargeModal} />
       <FavoriteIdol />
       <section>리스트</section>
     </main>
