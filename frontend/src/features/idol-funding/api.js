0e01ef3feee4f26936f059b4d7212da4f9e55b27
MELATONIN99 <sindogfood@naver.com>
2024-07-11 23:39:16 +0900

    feat. GetFundingApi 함수 추가

---
+++ frontend/src/features/idol-funding/api.js
@@ -0,0 +1,9 @@
+export async function getFundingApi() {
+  const response = await fetch(`https://fandom-k-api.vercel.app/8-7/donations?pageSize=10`);
+  if (response.ok) {
+    const result = await response.json();
+    return result;
+  } else {
+    alert("HTTP-Error: " + response.status);
+  }
+}
