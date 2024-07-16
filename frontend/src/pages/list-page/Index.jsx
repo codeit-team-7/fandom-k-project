7f786efcbbfd4534ab50742de1a04f2b0fb27232
전주현 <scv903@naver.com>
2024-07-16 14:39:49 +0900

    refactor : 리스트 페이지에서 충전 모달 코드를 크레딧 쪽으로 옮겼습니다

--- frontend/src/pages/list-page/Index.jsx
+++ frontend/src/pages/list-page/Index.jsx
@@ -1,24 +1,25 @@
-import { IdolFunding, IdolFundingModal, Credit, ChargeCredit, ChartOfTheMonth } from "@features";
+import {
+  IdolFunding,
+  IdolFundingModal,
+  Credit,
+  ChartOfTheMonth,
+} from "@features";
 import { useState } from "react";
 import { Header } from "../../features";
 
 export default function Index() {
-  const [chargeModal, setChargeModal] = useState(false);
   const [idolFundingModal, setIdolFundingModal] = useState(false);
-  const handleChargeModal = () => {
-    setChargeModal(chargeModal ? false : true);
 
-    document.body.style.overflow = chargeModal ? "auto" : "hidden";
-  };
   const handleIdolFundingModal = () => setIdolFundingModal((prev) => !prev);
 
   return (
     <main>
-      {chargeModal && <ChargeCredit onChargeClick={handleChargeModal} />}
       <Header />
-      <Credit onChargeClick={handleChargeModal} />
+      <Credit />
       <IdolFunding onFundingClick={handleIdolFundingModal} />
-      {idolFundingModal && <IdolFundingModal onFundingClick={handleIdolFundingModal} />}
+      {idolFundingModal && (
+        <IdolFundingModal onFundingClick={handleIdolFundingModal} />
+      )}
       <ChartOfTheMonth></ChartOfTheMonth>
     </main>
   );
