84efd8ab485a1903f566bba500c882125ed35aa1
MELATONIN99 <sindogfood@naver.com>
2024-07-18 02:05:49 +0900

    feat : API 호출 재시도 기능 구현

--- frontend/src/features/idol-funding/api.js
+++ frontend/src/features/idol-funding/api.js
@@ -1,9 +1,71 @@
+async function retryFetch(url, retries = 5, delay = 0) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const result = await response.json();
+      return result;
+    } else if (response.status >= 500 && retries > 0) {
+      console.warn(`Retrying fetch... Attempts left: ${retries}`);
+      await new Promise((resolve) => setTimeout(resolve, delay));
+      return retryFetch(url, retries - 1, delay);
+    } else {
+      throw new Error(`HTTP-Error: ${response.status}`);
+    }
+  } catch (error) {
+    if (retries > 0) {
+      console.warn(`Retrying fetch... Attempts left: ${retries}`);
+      await new Promise((resolve) => setTimeout(resolve, delay));
+      return retryFetch(url, retries - 1, delay);
+    } else {
+      alert(`Fetch failed after multiple attempts: ${error.message}`);
+      throw error;
+    }
+  }
+}
+
 export async function getFundingApi() {
-  const response = await fetch(`https://fandom-k-api.vercel.app/8-7/donations?pageSize=10`);
-  if (response.ok) {
-    const result = await response.json();
+  const url = `https://fandom-k-api.vercel.app/8-7/donations?pageSize=10`;
+  try {
+    const result = await retryFetch(url);
+    if (Array.isArray(result.list)) {
+      const newResult = result.list.filter((item) => item.status);
+      return newResult;
+    } else {
+      throw new Error("Invalid response format");
+    }
+  } catch (error) {
+    console.error("Failed to fetch funding data:", error);
+  }
+}
+
+export async function getRecheckApi(idolId) {
+  const url = `https://fandom-k-api.vercel.app/8-7/donations?pageSize=1&priorityIdolIds=${idolId}`;
+  try {
+    const result = await retryFetch(url);
     return result;
-  } else {
-    alert("HTTP-Error: " + response.status);
+  } catch (error) {
+    console.error("Failed to fetch recheck data:", error);
+  }
+}
+
+export async function putDonationsApi(id, creditUse) {
+  const url = `https://fandom-k-api.vercel.app/8-7/donations/${id}/contribute`;
+  try {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        amount: creditUse,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
   }
 }
